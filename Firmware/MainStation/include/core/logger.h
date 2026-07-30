81a7fbf828c88dc9f6b226b8ed8950a832575aec
archahmedelashmawy-del <archahmedelashmawy@gmail.com>
2026-07-30 09:46:14 +0300

    Update logger.h

--- Firmware/MainStation/include/core/logger.h
+++ Firmware/MainStation/include/core/logger.h
@@ -1,1 +1,47 @@
+#pragma once
 
+#include <Arduino.h>
+
+enum class LogLevel : uint8_t
+{
+    TRACE = 0,
+
+    DEBUG,
+
+    INFO,
+
+    WARNING,
+
+    ERROR,
+
+    CRITICAL
+};
+
+class Logger
+{
+public:
+
+    static void begin();
+
+    static void log(LogLevel level,
+                    const String& module,
+                    const String& message);
+
+    static void trace(const String& module,
+                      const String& message);
+
+    static void debug(const String& module,
+                      const String& message);
+
+    static void info(const String& module,
+                     const String& message);
+
+    static void warning(const String& module,
+                        const String& message);
+
+    static void error(const String& module,
+                      const String& message);
+
+    static void critical(const String& module,
+                         const String& message);
+};
