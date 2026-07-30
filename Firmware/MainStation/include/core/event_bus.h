f604ee7b0a2b030b3b6b6deeef9428cb18063c7f
archahmedelashmawy-del <archahmedelashmawy@gmail.com>
2026-07-30 21:51:17 +0300

    Update event_bus.h

--- Firmware/MainStation/include/core/event_bus.h
+++ Firmware/MainStation/include/core/event_bus.h
@@ -1,22 +1,47 @@
-enum class SystemEvent : uint8_t
+#ifndef EVENT_BUS_H
+#define EVENT_BUS_H
+
+#include <stddef.h>
+
+#include "config/constants.h"
+#include "core/event.h"
+
+/*
+==========================================================
+ Smart Dairy Platform
+ Event Bus
+----------------------------------------------------------
+ Central publish / subscribe mechanism.
+==========================================================
+*/
+
+using EventCallback = void (*)(const SystemEvent&);
+
+class EventBus
 {
-    RFID_DETECTED,
+public:
 
-    COW_VALIDATED,
+    static void initialize();
 
-    PUMP_STARTED,
+    [[nodiscard]] static bool subscribe(EventCallback callback);
 
-    PUMP_STOPPED,
+    [[nodiscard]] static bool unsubscribe(EventCallback callback);
 
-    MILKING_STARTED,
+    static void publish(const SystemEvent& event);
 
-    MILKING_FINISHED,
+    static void clear();
 
-    SD_WRITE_COMPLETED,
+    [[nodiscard]] static size_t subscriberCount();
 
-    ESPNOW_PACKET_RECEIVED,
+    [[nodiscard]] static bool isSubscribed(EventCallback callback);
 
-    ESPNOW_PACKET_SENT,
+private:
 
-    ERROR_OCCURRED
+    static EventCallback subscribers[SystemConstants::MAX_EVENT_SUBSCRIBERS];
+
+    static size_t count;
+
+    static bool isPublishing;
 };
+
+#endif
