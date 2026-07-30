9ead92b0ef5a2299d13aba8f22d0e46c295602d2
archahmedelashmawy-del <archahmedelashmawy@gmail.com>
2026-07-30 18:24:27 +0300

    Update espnow_driver.h

--- Firmware/MainStation/include/drivers/communication/espnow_driver.h
+++ Firmware/MainStation/include/drivers/communication/espnow_driver.h
@@ -1,1 +1,75 @@
+#ifndef ESPNOW_DRIVER_H
+#define ESPNOW_DRIVER_H
 
+#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_now.h>
+
+#include "core/error_codes.h"
+#include "config/packet.h"
+
+class ESPNowDriver
+{
+public:
+
+    using ReceiveCallback =
+        void (*)(const CommunicationPacket&);
+
+    using SendCallback =
+        void (*)(const uint8_t* mac,
+                 esp_now_send_status_t status);
+
+public:
+
+    ESPNowDriver();
+
+    ErrorCode begin();
+
+    bool isInitialized() const;
+
+    ErrorCode addPeer(const uint8_t* mac);
+
+    ErrorCode removePeer(const uint8_t* mac);
+
+    ErrorCode sendPacket(
+            const uint8_t* mac,
+            const CommunicationPacket& packet);
+
+    ErrorCode broadcast(
+            const CommunicationPacket& packet);
+
+    void registerReceiveCallback(
+            ReceiveCallback callback);
+
+    void registerSendCallback(
+            SendCallback callback);
+
+    uint8_t peerCount() const;
+
+private:
+
+    static void onReceive(
+        const esp_now_recv_info* info,
+        const uint8_t* data,
+        int len);
+
+    static void onSend(
+        const uint8_t* mac,
+        esp_now_send_status_t status);
+
+    static bool validatePacket(
+        const CommunicationPacket& packet,
+        int len);
+
+private:
+
+    bool initialized;
+
+    uint8_t peers;
+
+    static ReceiveCallback receiveCallback;
+
+    static SendCallback sendCallback;
+};
+
+#endif
