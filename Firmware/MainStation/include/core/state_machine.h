7e77cebe0232ef7edbeb6f3852df4ef8a4f2b66a
archahmedelashmawy-del <archahmedelashmawy@gmail.com>
2026-07-30 09:46:33 +0300

    Update state_machine.h

--- Firmware/MainStation/include/core/state_machine.h
+++ Firmware/MainStation/include/core/state_machine.h
@@ -1,1 +1,24 @@
+#pragma once
 
+#include "system_state.h"
+
+class StateMachine
+{
+public:
+
+    void begin(SystemState initialState);
+
+    void update();
+
+    void changeState(SystemState newState);
+
+    SystemState currentState() const;
+
+    SystemState previousState() const;
+
+private:
+
+    SystemState current = SystemState::BOOT;
+
+    SystemState previous = SystemState::BOOT;
+};
