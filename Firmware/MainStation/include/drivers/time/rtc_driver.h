2158387adea148e9858bc665eebe50bc04ad1edb
archahmedelashmawy-del <archahmedelashmawy@gmail.com>
2026-07-30 17:41:41 +0300

    Update rtc_driver.h

--- Firmware/MainStation/include/drivers/time/rtc_driver.h
+++ Firmware/MainStation/include/drivers/time/rtc_driver.h
@@ -1,1 +1,36 @@
+#ifndef RTC_DRIVER_H
+#define RTC_DRIVER_H
 
+#include <Arduino.h>
+#include <Wire.h>
+#include <RTClib.h>
+
+class RTCDriver
+{
+public:
+
+    RTCDriver();
+
+    bool begin();
+
+    bool isRunning() const;
+
+    bool adjust(const DateTime& dateTime);
+
+    DateTime now() const;
+
+    String getDateString() const;
+
+    String getTimeString() const;
+
+    String getTimestamp() const;
+
+private:
+
+    RTC_DS1307 rtc;
+
+    bool initialized;
+
+};
+
+#endif
