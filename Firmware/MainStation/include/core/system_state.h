7ed2704a99eea4684b88d0f27dea0b4e7141c2b7
archahmedelashmawy-del <archahmedelashmawy@gmail.com>
2026-07-30 09:47:13 +0300

    Update system_state.h

--- Firmware/MainStation/include/core/system_state.h
+++ Firmware/MainStation/include/core/system_state.h
@@ -1,1 +1,34 @@
+#pragma once
 
+#include <stdint.h>
+
+enum class SystemState : uint8_t
+{
+    BOOT = 0,
+
+    INITIALIZATION,
+
+    IDLE,
+
+    WAIT_FOR_IDENTIFICATION,
+
+    VALIDATE_COW,
+
+    PREPARE_MILKING,
+
+    PUMP_DELAY,
+
+    MILKING,
+
+    STOPPING,
+
+    SAVE_SESSION,
+
+    LOCAL_SYNC,
+
+    REMOTE_SYNC,
+
+    ERROR_STATE,
+
+    MAINTENANCE
+};
