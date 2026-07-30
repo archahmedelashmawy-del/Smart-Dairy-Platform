1671da81a8da458c6bfe0032a6df26cc0c671c99
archahmedelashmawy-del <archahmedelashmawy@gmail.com>
2026-07-30 18:04:57 +0300

    Update sd_driver.h

--- Firmware/MainStation/include/drivers/storage/sd_driver.h
+++ Firmware/MainStation/include/drivers/storage/sd_driver.h
@@ -7,35 +7,31 @@
 #include <SPI.h>
 
 #include "config/pins.h"
+#include "config/file_paths.h"
 #include "core/error_codes.h"
 
 class SDDriver
 {
 public:
-
     SDDriver();
 
     ErrorCode begin();
-
     bool isMounted() const;
-
     bool exists(const char* path) const;
 
     ErrorCode createFile(const char* path);
-
     ErrorCode removeFile(const char* path);
+    ErrorCode append(const char* path, const String& data);
+    ErrorCode write(const char* path, const String& data);
+    ErrorCode read(const char* path, String& output);
 
-    ErrorCode append(const char* path,
-                     const String& data);
-
-    ErrorCode write(const char* path,
-                    const String& data);
-
-    ErrorCode read(const char* path,
-                   String& output);
+    // الإضافات الجديدة
+    ErrorCode ensureDirectory(const char* path);
+    uint64_t cardSizeMB() const;
+    uint64_t totalSpaceMB() const;
+    uint64_t usedSpaceMB() const;
 
 private:
-
     bool mounted;
 };
 
