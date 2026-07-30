2cf00a1bc040c3a9e97c84c62f8edd318bad1a3a
archahmedelashmawy-del <archahmedelashmawy@gmail.com>
2026-07-30 09:34:19 +0300

    Update version.h

--- Firmware/MainStation/include/config/version.h
+++ Firmware/MainStation/include/config/version.h
@@ -1,1 +1,17 @@
+#pragma once
 
+namespace Version {
+
+constexpr char PROJECT_NAME[] = "Smart Dairy Platform";
+
+constexpr char DEVICE_NAME[] = "Main Station";
+
+constexpr char FIRMWARE[] = "2.0.0";
+
+constexpr char HARDWARE[] = "1.0";
+
+constexpr char COMM_PROTOCOL[] = "SDP-PROTOCOL-1.0";
+
+constexpr uint16_t BUILD_NUMBER = 1;
+
+}
