1e9021513c5fb61dd27418b7498bed5a90f7f12b
archahmedelashmawy-del <archahmedelashmawy@gmail.com>
2026-07-31 19:33:10 +0300

    Update communication_event.h

--- Firmware/MainStation/include/models/communication_event.h
+++ Firmware/MainStation/include/models/communication_event.h
@@ -1,1 +1,17 @@
+#ifndef COMMUNICATION_EVENT_H
+#define COMMUNICATION_EVENT_H
 
+#include "drivers/communication/espnow_driver.h"
+
+/*
+==========================================================
+ Smart Dairy Platform
+ Communication Event Layer (Clean Architecture)
+----------------------------------------------------------
+ Decouples upper-level services from lower-level drivers.
+==========================================================
+*/
+
+using CommunicationEvent = ReceivedPacket;
+
+#endif // COMMUNICATION_EVENT_H
